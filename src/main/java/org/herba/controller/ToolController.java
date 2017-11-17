26ecc61b3f03a2a54f3fcfdbe9b80d940d68839c
gh950219la <la9249@163.com>
2017-11-18 00:33:49 +0800

    文件上传后台功能

--- src/main/java/org/herba/controller/ToolController.java
+++ src/main/java/org/herba/controller/ToolController.java
@@ -1,10 +1,37 @@
 package org.herba.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.herba.util.FileUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+/**
+ * 工具控制器
+ * Created by gravel on 2017/11/17.
+ */
 @RestController
 @CrossOrigin
 public class ToolController {
-    
-}
+    //处理文件上传
+    @RequestMapping(value="/uploadFile", method = RequestMethod.POST)
+    public @ResponseBody
+    String uploadFile(HttpServletRequest request) {
+        List<MultipartFile> files = ((MultipartHttpServletRequest) request).getFiles("file");
+        String filePath = "src/main/resources/uploadFiles/";
+        for (MultipartFile file: files) {
+            if (!file.isEmpty()) {
+                try {
+                    String fileName = file.getOriginalFilename();
+                    FileUtils.uploadFile(file.getBytes(), filePath, fileName);
+                } catch (Exception e) {
+                    return "{'code':'0','msg':'上传失败，"+e.getMessage()+"' }";
+                }
+            }
+        }
+        return "{'code':'1','msg':'上传成功！' }";
+    }
+
+    }
